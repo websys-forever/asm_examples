df52ab133d913bb56787851e397d87f4e52f2edc
Evgenii <websys.forever@gmail.com>
2021-11-02 11:18:40 +0500

    Add get input name and print it

---
+++ get-input.asm
@@ -0,0 +1,29 @@
+%include 'functions.asm'
+
+section .data
+msg1 db 'Please enter your name: ', 0h
+msg2 db 'Hello, ', 0h
+
+section .bss
+sinput: resb 255
+
+section .text
+global _start
+
+_start:
+    mov eax, msg1
+    call sprint
+
+    mov edx, 255
+    mov ecx, sinput
+    mov ebx, 0
+    mov eax, 3
+    int 80h
+
+    mov eax, msg2
+    call sprint
+
+    mov eax, sinput
+    call sprint
+
+    call quit
