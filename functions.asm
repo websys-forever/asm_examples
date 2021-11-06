7854d6efb657834da42c4dab17c77e45983c3832
Evgenii <websys.forever@gmail.com>
2021-11-06 10:25:32 +0500

    Add input numbers calculator

--- functions.asm
+++ functions.asm
@@ -111,3 +111,43 @@ iprintLF:
     pop  eax
     pop  eax
     ret
+
+;------------------------------------------
+; int atoi(integer number)
+; Ascii to integer (atoi)
+atoi:
+    push ebx
+    push ecx
+    push edx
+    push esi
+    mov  esi, eax
+    mov  eax, 0
+    mov  ecx, 0
+
+.multiplyLoop:
+    xor  ebx, ebx
+    mov  bl, [esi+ecx]
+    cmp  bl, 48
+    jl   .finished
+    cmp  bl, 57
+    jg   .finished
+
+    sub  bl, 48
+    add  eax, ebx
+    mov  ebx, 10
+    mul  ebx
+    inc  ecx
+    jmp  .multiplyLoop
+
+.finished:
+    cmp  ecx, 0
+    je   .restore
+    mov  ebx, 10
+    div  ebx
+
+.restore:
+    pop  esi
+    pop  edx
+    pop  ecx
+    pop  ebx
+    ret
